eb90bb727b0fa92b28aae6ff37e18ebe68b8579d
Danya D. Smith <danyadsmith@gmail.com>
2017-04-07 23:09:29 -0400

    Create Configuration File
    This file is used to store all environment variables and
    configuration variables used in this application. Configuration
    data is separated into sections, including SERVER, DATABASE, API,
    etc.
    
    * The debug flag can be used to turn on or off diagnostic logging
    * When possible, prefer environment variables to hardcoded values

---
+++ config/config.js
@@ -0,0 +1,9 @@
+// SERVER
+exports.port = process.env.PORT || 3000;
+exports.debug = true;
+
+// DATABASE CONFIGURATION
+exports.dbName = process.env.SIREN_DB_DEV;
+exports.dbUrl = process.env.DATABASE_URL;
+exports.dbUser = process.env.POSTGRES_USER;
+exports.dbPwd = process.env.POSTGRES_PWD;
