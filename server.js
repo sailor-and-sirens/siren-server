fbfd4592f70466af4cad8d248f43f9cbd7259f79
Danya D. Smith <danyadsmith@gmail.com>
2017-04-07 23:18:03 -0400

    Create Express Server
    Initialize a basic Express server using bodyParser. Include chalk
    for styled log messages. Import configuration variables from the
    config.js file.
    
    Sets up HTTP headers and basic error-handling middleware.
    
    Listens on PORT 3000. To revise the port assignment, edit config.js.

---
+++ server.js
@@ -0,0 +1,31 @@
+const chalk = require('chalk');
+const config = require('./config/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./config/db');
+
+app.use(function (req, res, next) {
+  res.header('access-control-allow-origin', '*');
+  res.header('access-control-allow-methods', 'GET, POST, PUT, OPTIONS, DELETE');
+  res.header('access-control-allow-headers', 'content-type, accept');
+  res.header('access-control-max-age', 10);
+  res.header('x-xss-protection', 0);
+  next();
+});
+
+app.use(express.static('client'));
+
+app.use(function (err, req, res, next) {
+  console.error(chalk.red.bold('ERROR: ', err));
+  res.send(err);
+  next();
+});
+
+app.listen(config.port, function () {
+  console.log('Listening on port ' + config.port);
+});
