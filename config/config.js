7c5d3a2f02cf9d35f38d2de5b9be1f687a6bb813
Danya D. Smith <danyadsmith@gmail.com>
2017-04-11 03:14:16 -0400

    Update Configuration File to Include Facebook App ID
    In order to use Facebook Integration/Authentication with Expo, we
    need to register a developer account with Facebook and request an
    App ID.
    
    For security, we will store the Facebook App ID in an environment
    variable called SIREN_FB_APP_ID.

--- config/config.js
+++ config/config.js
@@ -7,3 +7,6 @@ exports.dbName = process.env.SIREN_DB_DEV;
 exports.dbUrl = process.env.DATABASE_URL;
 exports.dbUser = process.env.POSTGRES_USER;
 exports.dbPwd = process.env.POSTGRES_PWD;
+
+// FACEBOOK INTEGRATION
+exports.fbAppId = process.env.SIREN_FB_APP_ID;
