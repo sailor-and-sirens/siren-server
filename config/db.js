26ad55e28681871d4d7668375bd874a1942b1142
Danya D. Smith <danyadsmith@gmail.com>
2017-04-11 03:17:34 -0400

    Updates Auto-Generated Configuration for Sequelize CLI
    When Sequelize CLI is initialized using the command sequelize init,
    a configuration file is generated. The file initially is in JSON
    format, and attempting to run a migration with the file in its
    original form was failing.
    
    In addition, I wanted to be able to use environment variables in
    the configuration file, so I created the initialization such that
    it generates a JavaScript file (instead of JSON).
    
    To work around the errors that were being thrown while attempting
    to run migrations, I converted the generated JSON object to
    JavaScript and added `module.exports =` at the beginning of the
    file.
    
    Your development environment should be configured to include:
    
    * An environment variable named POSTGRES_USER
    * An environment variable named SIREN_DB_DEV
    
    A null password is assumed.

--- config/db.js
+++ config/db.js
@@ -1,11 +1,23 @@
-const chalk = require('chalk');
-const config = require('./config');
-const Postgres = require('pg');
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(config.dbName, config.POSTGRES_USER, config.POSTGRES_PWD, {
-  dialect: 'postgres'
-});
-
-console.log(chalk.green.bold('PostgreSQL connection established successfully!'));
-console.log(chalk.green('PostgreSQL Using Database: ', config.dbName));
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: null,
+    database: process.env.SIREN_DB_DEV,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+};
