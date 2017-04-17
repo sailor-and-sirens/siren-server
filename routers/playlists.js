e48e6c7182b361907f28114feba5fb01c40d418a
Emilie <emiliehester@gmail.com>
2017-04-17 17:42:48 -0600

    Implement remove episode from playlist functionality

--- routers/playlists.js
+++ routers/playlists.js
@@ -2,5 +2,6 @@ const playlistRouter = require('express').Router();
 const playlistController = require('../controllers/playlistController.js');
 
 playlistRouter.post('/add-episode', playlistController.addEpisodeToPlaylist);
+playlistRouter.delete('/remove-episode', playlistController.removeEpisodeFromPlaylist);
 
 module.exports = playlistRouter;
