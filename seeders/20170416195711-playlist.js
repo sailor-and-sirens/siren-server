23646f0ebebf87c68b44ef26c7e44ba12446a5f6
Emilie Hester <livfwd@users.noreply.github.com>
2017-04-17 18:41:54 -0600

    Update 20170416195711-playlist.js

--- seeders/20170416195711-playlist.js
+++ seeders/20170416195711-playlist.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: function (queryInterface) {
     return queryInterface.bulkInsert('Playlists', [
-      { name: 'Favorites', UserId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Bookmarks', UserId: 1, createdAt: new Date(), updatedAt: new Date() },
       { name: 'Listening To', UserId: 1, createdAt: new Date(), updatedAt: new Date()}
     ], {});
   },
