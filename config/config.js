f1dbd32586e31740ed4dc030fc59a78de112bf28
Danya D. Smith <danyadsmith@gmail.com>
2017-04-19 22:18:46 -0400

    Alter Configuration Settings for Local Development

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // SERVER
 exports.port = process.env.PORT || 3000;
-exports.debug = false;
+exports.debug = true;
 exports.log = true;
 
 // DATABASE CONFIGURATION
@@ -9,7 +9,7 @@ exports.dbName = process.env.SIREN_DB_PROD || process.env.SIREN_DB_DEV;
 exports.dbUrl = process.env.DATABASE_URL;
 exports.dbUser = process.env.POSTGRES_USER;
 exports.dbPwd = process.env.POSTGRES_PWD;
-exports.dbChangeSchema = true;
+exports.dbChangeSchema = false;
 exports.dbForceSync = {force: true};
 
 // FACEBOOK INTEGRATION
