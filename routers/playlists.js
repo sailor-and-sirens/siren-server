97e6d6cb0d611ffa3e544a79e04ec2822c550f12
Bryce Dooley <brycehdooley@gmail.com>
2017-04-20 15:14:35 -0400

    Revert "getPlaylists route"

--- routers/playlists.js
+++ routers/playlists.js
@@ -4,11 +4,6 @@ const playlistController = require('../controllers/playlistController.js');
 playlistRouter.get('/add-playlist-modal', playlistController.getPlaylistsForAddPlaylistModal);
 playlistRouter.post('/create-playlist', playlistController.createPlaylist);
 playlistRouter.post('/add-episode', playlistController.addEpisodeToPlaylist);
-playlistRouter.post('/create-playlist', playlistController.createPlaylist);
-playlistRouter.delete('/remove-playlist', playlistController.removePlaylist);
 playlistRouter.delete('/remove-episode', playlistController.removeEpisodeFromPlaylist);
-playlistRouter.get('/playlist-episodes', playlistController.getEpisodesFromPlaylist);
-playlistRouter.post('/get-playlists', playlistController.getPlaylists);
-playlistRouter.put('/update-title', playlistController.updatePlaylistTitle);
 
 module.exports = playlistRouter;
