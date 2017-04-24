3b092fba974077d450dee8f645c66854b7329017
Danya D. Smith <danyadsmith@gmail.com>
2017-04-24 11:30:54 -0400

    Add DELETE route for Episode Subscriptions
    Deletes Episode from UserEpisodes with matching UserID and
    EpisodeID

--- routers/episodes.js
+++ routers/episodes.js
@@ -4,5 +4,6 @@ const episodeController = require('../controllers/episodeController.js');
 episodeRouter.post('/', episodeController.subscribe);
 episodeRouter.put('/user-episode', episodeController.updateUserEpisode);
 episodeRouter.delete('/user-episode-inbox', episodeController.removeEpisodeFromInbox);
+episodeRouter.delete('/:id', episodeController.deleteUserEpisode);
 
 module.exports = episodeRouter;
