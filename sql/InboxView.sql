af8c97b81e7897c2758dcb3f6597d6244868a6ca
Emilie <emiliehester@gmail.com>
2017-04-25 08:31:01 -0600

    Add currentTime to Inbox view

--- sql/InboxView.sql
+++ sql/InboxView.sql
@@ -3,7 +3,8 @@ SELECT
   "Users"."id" as "UserId", "Users"."username", "Users"."email", "Users"."avatarUrl",
   "Podcasts"."id" as "PodcastId", "Podcasts"."name" as "title", "Podcasts"."artistName" as "creator", "Podcasts"."feedUrl", "Podcasts"."primaryGenreName" as "tag", "Podcasts"."artworkUrl" as "image", "Podcasts"."artworkUrl600" as "image600",
   "Episodes"."id" as "EpisodeId", "Episodes"."title" as "episodeTitle", "Episodes"."description", "Episodes"."length", "Episodes"."releaseDate", "Episodes"."url", "Episodes"."feed",
-  "UserEpisodes"."liked", "UserEpisodes"."bookmarked" as "bookmark"
+  "UserEpisodes"."liked", "UserEpisodes"."bookmarked" as "bookmark",
+  "UserEpisodes"."currentTime"
 FROM
   "Podcasts", "Episodes", "UserEpisodes", "Users"
 WHERE
